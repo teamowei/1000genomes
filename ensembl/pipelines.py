131671a197e1b64d2b9a4db11a5d3e49c315b921
teamowei <ranleiw@gmail.com>
2018-07-27 17:26:52 +0800

    fix

--- ensembl/pipelines.py
+++ ensembl/pipelines.py
@@ -15,7 +15,7 @@ class EnsemblPipeline(object):
         self.ws.append(['rsid', 'riskAllele','f_r_cdx','f_rr_cdx','f_nn_cdx','f_rn_cdx','f_r_chb','f_rr_chb',	'f_nn_chb','f_rn_chb','f_r_chs','f_rr_chs','f_nn_chs',	'f_rn_chs','f_r_eas','f_rr_eas','f_nn_eas','f_rn_eas'])
 
     def process_item(self, item, spider):
-        line = [item['rsid'], item['r_cdx'],item['rr_cdx'],item['nn_cdx'],item['rn_cdx'],item['r_chb'],item['rr_chb'],item['nn_chb'],item['rn_chb'],item['r_chs'],item['rr_chs'],item['nn_chs'],item['rn_chs'],item['r_eas'],item['rr_eas'],item['nn_eas'],item['rn_eas'] ]
+        line = [item['rsid'], item['risk_allele'],item['r_cdx'],item['rr_cdx'],item['nn_cdx'],item['rn_cdx'],item['r_chb'],item['rr_chb'],item['nn_chb'],item['rn_chb'],item['r_chs'],item['rr_chs'],item['nn_chs'],item['rn_chs'],item['r_eas'],item['rr_eas'],item['nn_eas'],item['rn_eas'] ]
         self.ws.append(line)
         return item
 
--- ensembl/spiders/ensemblspider.py
+++ ensembl/spiders/ensemblspider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+import operator
 import ensembl.openxls as op
 from selenium import webdriver
 from ensembl.items  import EnsemblItem,RaceItem
@@ -30,100 +31,111 @@ class EnsemblspiderSpider(scrapy.Spider):
         #     InfoTable=Info1.xpath('./')
         # pass
         # Info1=response.xpath('//h2[contains(text(),"1000 Genomes Project Phase 3 allele frequencies")]').extract()[0]
-
-        Info2 = response.xpath('normalize-space(//span[@style="font-family:Courier,monospace;white-space:nowrap;margin-left:5px;padding:2px 4px;background-color:#F6F6F6"])')
-        baseInfo = Info2.extract()[0].split('\xa0\xa0')
-        rsid=baseInfo[2]
-        ref_alleles= baseInfo[3].split(',')
-        ALT_alleles=baseInfo[4].split(",")
-        r_allele1=self.find_risk_allele(rsid,rsidInfos)
-        r_allele2=self.allele_reverse(r_allele1)
-        n_alleles=[ ]
-        nn_genotype=[ ]
-        rn_genotype=[ ]
-        rr_genotype=[]
-        riskAllele =[ ]
-
-
-
-        if r_allele1 in ref_alleles or r_allele2 in ref_alleles:
-            riskAllele.extend(ref_alleles)
-            n_alleles.extend(ALT_alleles)
-
-        elif r_allele1 in ALT_alleles or r_allele2 in ALT_alleles:
-            riskAllele.extend(ALT_alleles)
-            n_alleles.extend(ref_alleles)
-
-
-        else:
-            print("risk alleles not in web")
-
-        for n,p in zip(n_alleles,riskAllele):
-            nn_genotype.append(n+"\|"+n)
-            rn_genotype.append(n+"\|"+p)
-            rn_genotype.append(p+"\|"+n)
-            rr_genotype.append(p+"\|"+p)
+        try:
+            Info2 = response.xpath('normalize-space(//span[@style="font-family:Courier,monospace;white-space:nowrap;margin-left:5px;padding:2px 4px;background-color:#F6F6F6"])')
+            baseInfo = Info2.extract()[0].split('\xa0\xa0')
+            rsid=baseInfo[2]
+            ref_alleles= baseInfo[3].split(',')
+            ALT_alleles=baseInfo[4].split(",")
+            r_allele1=self.find_risk_allele(rsid,rsidInfos)
+            r_allele2=self.allele_reverse(r_allele1)
+            n_alleles=[]
+            nn_genotype=[]
+            rn_genotype=[]
+            rr_genotype=[]
+            riskAllele =[]
 
 
 
+            if r_allele1 in ref_alleles and r_allele2 in ref_alleles:
+                riskAllele.extend(ref_alleles)
+                n_alleles.extend(ALT_alleles)
+            elif r_allele1 in ref_alleles and r_allele1 not in ALT_alleles:
+                riskAllele.extend()
+            elif r_allele1 in ALT_alleles and r_allele2 in ALT_alleles:
+                riskAllele.extend(ALT_alleles)
+                n_alleles.extend(ref_alleles)
+            elif r_allele1 in ALT_alleles and r_allele2 not in ALT_alleles:
+                riskAllele.append(r_allele1)
+                n_alleles.extend(ref_alleles).extend(ALT_alleles.remove(r_allele1))
+            elif r_allele1 not in ALT_alleles and r_allele2  in ALT_alleles:
+                riskAllele.append(r_allele2)
+                n_alleles.extend(ref_alleles).extend(ALT_alleles.remove(r_allele2))
 
-        driver=webdriver.PhantomJS(executable_path='C:/Users/Wei/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
-        driver.get(response.url+'#1000genomesprojectphase3_table')
-        htmls = driver.page_source.replace("\n","").replace("\r","")
-        endText_freq="\\(.*?\\) </div>"
-        races_name=['EAS','CDX','CHB','CHS']
-        #race_freqs=[]
-        race_freq_dic={}
-        for r in races_name:
+            else:
+                print("risk alleles not in web")
 
-            r_pattern='<span class="_ht ht"><b>'+r+'</b></span>(.*?)<span class="open">Hide</span>'
+            for n in n_alleles:
+                for p in riskAllele:
+                    nn_genotype.append(n+"\|"+n)
+                    rn_genotype.append(n+"\|"+p)
+                    rn_genotype.append(p+"\|"+n)
+                    rr_genotype.append(p+"\|"+p)
 
-            startText_genotype = "</b>:"
-            startText_allele = "</span></b>:"
-            pattern = re.compile(r_pattern)
-            result=pattern.search(htmls)
-            #riskf,rn_fre,nn_fre,r_fre,n_fre=0,0,0,0,0
+            nn_genotype=list(set(nn_genotype))
+            rn_genotype=list(set(rn_genotype))
+            rr_genotype=list(set(rr_genotype))
 
-            if result:
-                Info1=result.group(1)
-                riskFreqText=self.findText(Info1, riskAllele + "\|" + riskAllele + "</b>:", endText_freq)
-                riskf= riskFreqText.group(1).strip() if riskFreqText is not None else 0
-                rn_fre=self.max_allele_frequence(Info1,rn_genotype,startText_genotype,endText_freq)
-                nn_fre = self.max_allele_frequence(Info1, nn_genotype,startText_genotype,endText_freq)
-                r_FreqText = self.findText(Info1,riskAllele+"</span></b>:",endText_freq)
-                r_fre = r_FreqText.group(1).strip() if r_FreqText is not None else 0
-                #r_fre=self.findText(Info1,riskAllele+"</span></b>:",endText_freq).group(1).strip()
-                n_fre=self.max_allele_frequence(Info1,n_alleles,startText_allele,endText_freq)
-            race_freq = RaceItem(rsid,r_allele1,r_fre,n_fre,riskf,nn_fre,rn_fre)
-            #race_freqs.append(race_freq)
-            race_freq_dic[r]=race_freq
+            driver=webdriver.PhantomJS(executable_path='C:/Users/Wei/Downloads/phantomjs-2.1.1-windows/bin/phantomjs.exe')
+            driver.get(response.url+'#1000genomesprojectphase3_table')
+            htmls = driver.page_source.replace("\n","").replace("\r","")
+            endText_freq="\\(.*?\\) </div>"
+            races_name=['EAS','CDX','CHB','CHS']
+            #race_freqs=[]
+            race_freq_dic={}
+            for r in races_name:
 
-        item=EnsemblItem()
-        item['rsid']=rsid
-        item['risk_allele']=riskAllele
+                r_pattern='<span class="_ht ht"><b>'+r+'</b></span>(.*?)<span class="open">Hide</span>'
 
-        item['r_cdx']=race_freq_dic['CDX'].rf
-        item['rr_cdx']=race_freq_dic['CDX'].rrf
-        item['nn_cdx']=race_freq_dic['CDX'].nnf
-        item['rn_cdx']=race_freq_dic['CDX'].rnf
-        item['r_chb']=race_freq_dic['CHB'].rf
-        item['rr_chb']=race_freq_dic['CHB'].rrf
-        item['nn_chb']=race_freq_dic['CHB'].nnf
-        item['rn_chb']=race_freq_dic['CHB'].rnf
-        item['r_chs'] =race_freq_dic['CHS'].rf
-        item['rr_chs'] =race_freq_dic['CHS'].rrf
-        item['nn_chs'] =race_freq_dic['CHS'].nnf
-        item['rn_chs'] =race_freq_dic['CHS'].rnf
-        item['r_eas'] =race_freq_dic['EAS'].rf
-        item['rr_eas'] =race_freq_dic['EAS'].rrf
-        item['nn_eas'] =race_freq_dic['EAS'].nnf
-        item['rn_eas'] =race_freq_dic['EAS'].rnf
+                startText_genotype = "</b>:"
+                startText_allele = "</span></b>:"
+                pattern = re.compile(r_pattern)
+                result=pattern.search(htmls)
+                riskf,rn_fre,nn_fre,r_fre,n_fre=0,0,0,0,0
+
+                if result:
+                    Info1=result.group(1)
+
+                    # riskFreqText=self.findText(Info1, r1 + "\|" + r1 + "</b>:", endText_freq)
+                    # riskf= riskFreqText.group(1).strip() if riskFreqText is not None else 0
+                    riskf=self.max_allele_frequence(Info1,rr_genotype,startText_genotype,endText_freq)
+                    rn_fre=self.max_allele_frequence(Info1,rn_genotype,startText_genotype,endText_freq)
+                    nn_fre = self.max_allele_frequence(Info1, nn_genotype,startText_genotype,endText_freq)
+                    r_fre = self.max_allele_frequence(Info1,riskAllele,"</span></b>:",endText_freq)
+                    #r_fre = r_FreqText.group(1).strip() if r_FreqText is not None else 0
+                    n_fre=self.max_allele_frequence(Info1,n_alleles,"</span></b>:",endText_freq)
+                race_freq = RaceItem(rsid,r_allele1,r_fre,n_fre,riskf,nn_fre,rn_fre)
+                race_freq_dic[r]=race_freq
+
+            item=EnsemblItem()
+            item['rsid']=rsid
+            item['risk_allele']=r_allele1
+
+            item['r_cdx']=race_freq_dic['CDX'].rf
+            item['rr_cdx']=race_freq_dic['CDX'].rrf
+            item['nn_cdx']=race_freq_dic['CDX'].nnf
+            item['rn_cdx']=race_freq_dic['CDX'].rnf
+            item['r_chb']=race_freq_dic['CHB'].rf
+            item['rr_chb']=race_freq_dic['CHB'].rrf
+            item['nn_chb']=race_freq_dic['CHB'].nnf
+            item['rn_chb']=race_freq_dic['CHB'].rnf
+            item['r_chs'] =race_freq_dic['CHS'].rf
+            item['rr_chs'] =race_freq_dic['CHS'].rrf
+            item['nn_chs'] =race_freq_dic['CHS'].nnf
+            item['rn_chs'] =race_freq_dic['CHS'].rnf
+            item['r_eas'] =race_freq_dic['EAS'].rf
+            item['rr_eas'] =race_freq_dic['EAS'].rrf
+            item['nn_eas'] =race_freq_dic['EAS'].nnf
+            item['rn_eas'] =race_freq_dic['EAS'].rnf
 
 
-        # if Info1:
-        #     print(Info1)
-        # pass
-        yield item
+            # if Info1:
+            #     print(Info1)
+            # pass
+            yield item
+        except Exception as e:
+            print("Exception:"+e.message)
+            print(rsid + "is wrong")
 
 
 
